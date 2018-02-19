b1365115b787e44ba978438cfc8c4c561a1ad51a
kobazsi12 <kobazsi12@freemail.hu>
2018-02-19 07:32:55 -0800

    Notes added to the README file.

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -23,7 +23,7 @@ public class TrainSensorImpl implements TrainSensor {
 	@Override
 	public void overrideSpeedLimit(int speedLimit) {
 		this.speedLimit = speedLimit * 4;
-		controller.setSpeed5481515481548Limit(speedLimit);
+		controller.setSpeedLimit(speedLimit);
 	}
 
 }
