1d7ac2779e4cb597511530fc1d8343aa861ba24b
kobazsi12 <kobazs12@freemail.hu>
2018-03-05 06:11:52 -0800

    Joystick timer added

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,24 +2,32 @@ package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	private Timer speedTimer = new Timer();
 
 	@Override
 	public void followSpeed() {
 		if (referenceSpeed < 0) {
 			referenceSpeed = 0;
 		} else {
-		    if(referenceSpeed+step > 0) {
+		    if(referenceSpeed + step > 0) {
                 referenceSpeed += step;
             } else {
 		        referenceSpeed = 0;
             }
 		}
 
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		enforceSpeedLimit();
 	}
 
